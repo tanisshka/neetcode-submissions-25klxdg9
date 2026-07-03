cf917475ac1b23127396b2c17e50187077ef4cc5
Tanishka Patil <tanishkapatil0102@gmail.com>
2026-07-03 11:48:19 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    int partition(vector<int>& nums,int low,int high){
+        int pivot=nums[low];
+        int i=low;
+        int j=high;
+
+        while(i<j){
+            while(i<=high && nums[i]<=pivot){
+                i++;
+            }
+
+            while(j>=low && nums[j]>pivot){
+                j--;
+            }
+
+            if(i<j){
+                swap(nums[i],nums[j]);
+            }
+        }
+        swap(nums[low],nums[j]);
+        return j;
+    }
+    void QuickSort(vector<int>& nums,int low,int high){
+        if(low<high){
+            int pi=partition(nums,low,high);
+            QuickSort(nums,low,pi-1);
+            QuickSort(nums,pi+1,high);
+        }
+    }
+public:
+    void sortColors(vector<int>& nums) {
+        //Brute force approach -> Quicksort algorithm
+        QuickSort(nums,0,nums.size()-1);
+        return;
+    }
+};
