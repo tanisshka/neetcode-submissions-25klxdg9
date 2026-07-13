7e6578c82ca22c16c865731606675fc002241d2c
Tanishka Patil <tanishkapatil0102@gmail.com>
2026-07-13 20:17:01 +0530

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.cpp
@@ -0,0 +1,47 @@
+class Solution {
+   public:
+    vector<int> majorityElement(vector<int>& nums) {
+        // Extended Moore-Voting algorithm
+
+        int cnt1 = 0;
+        int el1 = INT_MIN;
+        int cnt2 = 0;
+        int el2 = INT_MIN;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (cnt1 == 0 && nums[i] != el2) {
+                cnt1 = 1;
+                el1 = nums[i];
+            } else if (cnt2 == 0 && nums[i] != el1) {
+                cnt2 = 1;
+                el2 = nums[i];
+            } else if (el1 == nums[i]) {
+                cnt1++;
+            } else if (el2 == nums[i]) {
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = cnt2 = 0;
+
+        for (int num : nums) {
+            if (num == el1)
+                cnt1++;
+            else if (num == el2)
+                cnt2++;
+        }
+
+        vector<int> ans;
+        int n = nums.size();
+
+        if (cnt1 > n / 3) ans.push_back(el1);
+
+        if (cnt2 > n / 3) ans.push_back(el2);
+
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
