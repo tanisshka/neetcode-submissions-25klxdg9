0a1a37f0aff88d49f259daca24f82748910ae823
Tanishka Patil <tanishkapatil0102@gmail.com>
2026-09-15 15:25:27 +0530

    Add: intersection-of-two-linked-lists - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-0.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        unordered_set<ListNode*> nodeSet;
+        ListNode* temp=headA;
+        while(temp){
+            nodeSet.insert(temp);
+            temp=temp->next;
+        }
+
+        temp=headB;
+        while(temp){
+            if(nodeSet.find(temp)!=nodeSet.end()){
+                return temp;
+            }
+            temp=temp->next;
+        }
+        return nullptr;
+
+    }
+};
